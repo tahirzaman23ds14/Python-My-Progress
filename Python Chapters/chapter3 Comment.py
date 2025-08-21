6ca56f0109ec9e790e78e1d30e0e25d533a14716
tahirzaman23ds14 <tahirzamanoffice1@gmail.com>
2025-08-21 12:13:03 +0500

    chapter 1,2,3

---
+++ Python Chapters/chapter3 Comment.py
@@ -0,0 +1,51 @@
+#In Python,
+#comments are notes in your code that the computer ignores.
+#They are just for humans to understand the code better.
+
+# There are two types of comments in Python:
+
+#1.  Single-line comment
+#    Use the # symbol.
+#    Everything after # on that line is ignored by Python.
+
+#    Example
+        #This is a single-line comment
+#        print("Hello, World!")  # This prints Hello, World!
+
+"""
+2. Multi-line comment (docstring style)
+
+Python does not have a special multi-line comment symbol.
+Instead, programmers use triple quotes 
+"""
+
+# OUt side of text  having triple quotes up and down 
+
+"""
+   Example of 
+   multi line comment
+   
+
+"""
+
+"""
+
+ hey There
+ i am here to learn
+ Python
+ Comment
+
+"""
+
+#  This is a multi-line comment.
+# You can write as many lines as you want.
+# Python will ignore this.
+
+
+print("Python comments example")
+
+
+#⚡ Tip:
+
+#
+# Use comments to explain why you wrote code, not what (because code itself shows what it’s doing).
