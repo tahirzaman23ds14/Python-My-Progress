cae2e924d2cf4d123f28a73c4884a748454b61b9
tahirzaman23ds14 <tahirzamanoffice1@gmail.com>
2025-08-21 13:17:38 +0500

    PRINT

--- Python Chapters/Chapter1 introduction.py
+++ Python Chapters/Chapter1 introduction.py
@@ -1,2 +1,4 @@
 #
-print("hey there")
+print("hey \"there\"")
+
+
---
+++ Python Chapters/chapter4 Print.py
@@ -0,0 +1,34 @@
+# print function in Python is used to display output to the console.
+
+print("python world")  # Using double quotes for the string             or 
+print('Hello, World!')  # Using single quotes for the string            both are valid
+
+
+print('MY NAME IS "Tahir"')  # Using single quotes to include double quotes in the string
+
+
+
+print("Hello, \"World!\"")  # Using escape character to include quotes in the string
+print("hey my name is \"Tahir\"")  
+
+
+print('hey \'there\'')  # Using escape character to include single quotes in the string
+      
+      
+print("path: C:\\User\\tahir_Zaman")     #backslash escape character to include backslash in the string
+
+
+
+# create a space between 2 print statements
+
+print("message 1")
+print("message 2")  # This will print on the same line as message 1
+
+
+print()# create a new line between 2 print statements using \n
+
+
+print("message 1 \n")
+print("message 2")  # This will print on a new line due to the newline character \n
+
+
