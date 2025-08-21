a3153766480c071173656da076fb67a608611275
tahirzaman23ds14 <tahirzamanoffice1@gmail.com>
2025-08-21 13:27:15 +0500

    Update chapter4 Print.py

--- Python Chapters/chapter4 Print.py
+++ Python Chapters/chapter4 Print.py
@@ -1,22 +1,25 @@
 # print function in Python is used to display output to the console.
 
-print("python world")  # Using double quotes for the string             or 
-print('Hello, World!')  # Using single quotes for the string            both are valid
+print("python world")  # Using double quotes for the string             or
+# Using single quotes for the string            both are valid
+print('Hello, World!')
 
 
-print('MY NAME IS "Tahir"')  # Using single quotes to include double quotes in the string
+# Using single quotes to include double quotes in the string
+print('MY NAME IS "Tahir"')
 
 
+# Using escape character to include quotes in the string
+print("Hello, \"World!\"")
+print("hey my name is \"Tahir\"")
 
-print("Hello, \"World!\"")  # Using escape character to include quotes in the string
-print("hey my name is \"Tahir\"")  
+
+# Using escape character to include single quotes in the string
+print('hey \'there\'')
 
 
-print('hey \'there\'')  # Using escape character to include single quotes in the string
-      
-      
-print("path: C:\\User\\tahir_Zaman")     #backslash escape character to include backslash in the string
-
+# backslash escape character to include backslash in the string
+print("path: C:\\User\\tahir_Zaman")
 
 
 # create a space between 2 print statements
@@ -25,10 +28,6 @@ print("message 1")
 print("message 2")  # This will print on the same line as message 1
 
 
-print()# create a new line between 2 print statements using \n
-
-
-print("message 1 \n")
-print("message 2")  # This will print on a new line due to the newline character \n
-
-
+print()  # create a new line between 2 print statements using \n
+# This will print message 1 and message 2 on separate lines
+print("message 1\nmessage 2")
