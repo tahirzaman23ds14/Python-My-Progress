613df6ebd985afd1625ce43b785dd8f9d42d36f8
tahirzaman23ds14 <tahirzamanoffice1@gmail.com>
2025-08-22 16:57:30 +0500

    Update Chapter5 Variable.py

--- Python Chapters/Chapter5 Variable.py
+++ Python Chapters/Chapter5 Variable.py
@@ -2,3 +2,26 @@
 
 
 # how varibale works
+
+
+
+# practice with variable
+
+
+print("My name is Tahir")
+print("Tahir is learning Python")
+print("Tahir want to become a Python Expert")
+
+    # if i Replace Tahir with some other name
+    # thats a lot of work
+    # instead we can use variable
+
+###### Let's use variables to make our code more efficient and easier to maintain.
+
+name = "Ali"  # Assigning the "Ali"  to the variable 'name' # or you can use any name instead of Ali
+
+print("My name is", name)  # Using the variable 'name' in the print statement
+print(name, "is learning Python")  # Using the variable 'name' in the print statement
+print(name, "want to become a Python Expert")  # Using the variable 'name' in the print statement
+
+
