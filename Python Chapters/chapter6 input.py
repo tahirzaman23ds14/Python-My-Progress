8c4df96c1500117a4c4c79836c98f4e382607215
tahirzaman23ds14 <tahirzamanoffice1@gmail.com>
2025-08-25 18:02:02 +0500

    Update chapter6 input.py

--- Python Chapters/chapter6 input.py
+++ Python Chapters/chapter6 input.py
@@ -1,12 +1,7 @@
-"""What input() does
-
- 1 Waits for the user to type something and press Enter.
-
- 2 Returns a string (type str).
-
- 3 Removes the trailing newline that you typed when you hit Enter.
-
- 4 Signature: input(prompt=None)
 """
-name = input("Enter your name: ")   # prompt is optional
-print(type(name))                   # <class 'str'>
+ What is input() in Python?
+
+The input() function is used to take input from the user.
+
+It pauses the program, waits for the user to type something, and then returns that as text (string).
+"""
