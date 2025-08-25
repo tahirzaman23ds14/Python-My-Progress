d3257541d5ea9d277a4250f6a7df41822b8a2adb
tahirzaman23ds14 <tahirzamanoffice1@gmail.com>
2025-08-25 18:03:07 +0500

    Update chapter6 input.py

--- Python Chapters/chapter6 input.py
+++ Python Chapters/chapter6 input.py
@@ -5,3 +5,14 @@ The input() function is used to take input from the user.
 
 It pauses the program, waits for the user to type something, and then returns that as text (string).
 """
+
+##Basic Example
+name = input("Enter your name: ")
+print("Hello,", name)
+
+
+# When you run it:
+""" 
+Enter your name: Tahir
+Hello, Tahir
+"""
